c699ece2079e36b5126ec83305b892aec4c95b66
cosmicCoder2003 <vednarsekar@gmail.com>
2025-03-18 20:27:31 +0530

    all launch packages combined

--- ros2_Ws/run.sh
+++ ros2_Ws/run.sh
@@ -11,13 +11,24 @@ ros2 launch realsense2_camera rs_launch.py color_fps:=15 depth_fps:=15\ &
 # fix the missing TF transform
 ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link camera_link &
 # launch RTAB Map
-ros2 launch rtabmap_launch rtabmap.launch.py \
+# uncomment for normal launch
+# ros2 launch rtabmap_launch rtabmap.launch.py \
+#    rgb_topic:=/camera/camera/color/image_raw \
+#    depth_topic:=/camera/camera/depth/image_rect_raw \
+#    camera_info_topic:=/camera/camera/color/camera_info \
+#    approx_sync:=true approx_sync_max_interval:=0.02\
+#    rtabmap_args:="--Vis/DepthAsMask false --RGBD/Decimation 2" &
+# the master controller node starts at the last
+
+# launch RTAB Map with clearing previous session
+rm -f ~/.ros/rtabmap.db && ros2 launch rtabmap_launch rtabmap.launch.py \
    rgb_topic:=/camera/camera/color/image_raw \
    depth_topic:=/camera/camera/depth/image_rect_raw \
    camera_info_topic:=/camera/camera/color/camera_info \
    approx_sync:=true approx_sync_max_interval:=0.02\
    rtabmap_args:="--Vis/DepthAsMask false --RGBD/Decimation 2" &
-# the master controller node starts at the last
+
+# launch the master controller
 ros2 launch my_robot_launcher robot.launch.py &
 wait
 echo "robot launcher has terminated succesfully"
