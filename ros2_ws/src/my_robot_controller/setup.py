1b505f7ad9cf7236201a1d5c175c1ba4e964f545
Newt-Shadow <anmol.s.sahoo@gmail.com>
2025-04-16 00:51:25 +0530

    new ui

--- ros2_ws/src/my_robot_controller/setup.py
+++ ros2_ws/src/my_robot_controller/setup.py
@@ -13,13 +13,39 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='nits',
-    maintainer_email='vednarsekar@gmail.com',
+    maintainer='rahul',
+    maintainer_email='rahul@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-'flat_area_processor =my_robot_controller.flat_area_processor:main',        ],
+            # "position_node = my_robot_controller.position_publisher:main",
+            
+            # "path_planner_node = my_robot_controller.path_planner:main",
+            # "path_planner_server_node = my_robot_controller.path_planner_server:main",
+            # "path_planner_simulation_node = my_robot_controller.path_planner_simulation:main",
+            
+            # "path_planner_simulation_server_node = my_robot_controller.path_planner_simulation_server:main"            
+            
+            "bridge_server = my_robot_controller.bridge_server:main",
+            "bridge_server_combined = my_robot_controller.bridge_server_combined:main",
+            # "path_mover = my_robot_controller.path_mover:main",
+            "path_planner = my_robot_controller.path_planner:main",
+            "path_manual = my_robot_controller.path_manual:main",
+            "mode_controller = my_robot_controller.mode_controller:main",
+            "launch_checker = my_robot_controller.launch_monitor:main",
+            "coordinate = my_robot_controller.coordinate:main",
+            "hover = my_robot_controller.coordinate:main",
+            "perpendicular_webcam = my_robot_controller.perpendicular_webcam:main",
+            "height_publisher = my_robot_controller.height:main",
+
+            # replace with Shwetangshu and Mimansa's modules  
+            "flat_area = my_robot_controller.flat_area:main",
+            # "boundary_mapper = my_robot_controller.boundary_mapper:main",
+            "odom = my_robot_controller.odom:main",
+            "boundary_mapper = my_robot_controller.boundary_mapper:main",
+            
+        ],
     },
 )
