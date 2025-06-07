5dc7f9d904b0f5ba2d03ca1d3631b0364e17d84a
Debashish Buragohain <debashishburagohain000@gmail.com>
2025-06-07 11:32:50 +0530

    unused folders removed

--- ros2_Ws/build/my_drone_launcher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/f/NITS/ANAV/main/ros2_Ws/src/my_drone_launcher
-# Build directory: /mnt/f/NITS/ANAV/main/ros2_Ws/build/my_drone_launcher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros2_Ws/build/my_robot_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/f/NITS/ANAV/main/ros2_Ws/install/my_robot_controller'
